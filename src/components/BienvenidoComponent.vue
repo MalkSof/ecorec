29c73430350764a42f0d53144fb7278cbb7bdbe6
matikalo <matikalo@hotmail.com>
2021-08-15 18:45:24 -0300

    Nuestros Productos

--- src/components/BienvenidoComponent.vue
+++ src/components/BienvenidoComponent.vue
@@ -1,19 +1,69 @@
 <template>
   <div>
-  <div id="bienvenido">
-    <div id="Bienvenido_a_productos_ECOREC">
-      Bienvenido a productos ECOREC
+    <div id="bienvenido">
+      <div id="Bienvenido_a_productos_ECOREC">
+        Bienvenido a productos ECOREC
+      </div>
     </div>
-  </div>
-  <div style="top: 450px;">
-    <div id="Lorem_ipsum">
-      Lorem ipsum
+    <div style="top: 450px;">
+      <div id="Lorem_ipsum">
+        Lorem ipsum
+      </div>
+      <div id="Lorem_ipsum_dolor_sit_amet_con">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet
+      </div>
     </div>
-
-    <div id="Lorem_ipsum_dolor_sit_amet_con">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet
+    <div id="rect_1">
+      <div class="container">
+          <div class="row">
+            <div class="col-md-4 col-12">
+                <div class="margen">
+                  <div>
+                      <div>
+                          <img src="envios.png">
+                      </div>
+                  </div>
+                  <div id="Envos">
+                      Envíos
+                  </div>
+                  <div id="Lorem_ipsum_dolor_sit_amet_con_">
+                      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-12">
+                <div>
+                    <img src="pagos.png">
+                </div>
+                <div id="Pagos">
+                    Pagos
+                </div>
+                <div id="Lorem_ipsum_dolor_sit_amet_con_ba">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                </div>
+            </div>
+            <div class="col-md-4 col-12">
+                <div>
+                  <img src="atencion.png">
+                </div>
+                <div id="Atencin">
+                    Atención
+                </div>
+                <div id="Lorem_ipsum_dolor_sit_amet_con_bh">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
-  </div>
+    <div>
+      <div id="Nuestros_prod">
+        Nuestros productos
+      </div>
+      <div id="Seleccione">
+        Seleccione su industria de interés
+      </div>
+    </div>
   </div>
 </template>
 
