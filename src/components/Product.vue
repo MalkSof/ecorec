2148169dac779e008426dc6272368e4e7b011e74
MalkSof <42155335+MalkSof@users.noreply.github.com>
2021-08-17 15:05:28 -0300

    enviarProducto y estilos

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -54,21 +54,17 @@
       </div>
       <div class="col-sm-5 col-12">
         <div id="tituloProducto">
-            Titulo
+
+          {{productoSeleccionado.titulo}}
+        <br>
         </div>
+        <br>
         <div class="subtitulo">
             Caracteristicas
         </div>
         <div class="textoNormal">
-
-
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-          et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-          <br>em ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-          no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>Lorem ipsum dolor
-          sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+          <br>
+          {{productoSeleccionado.descri}}
         </div>
       </div>
     </div>
