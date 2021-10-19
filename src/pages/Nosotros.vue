afb6c3c545c6d3dcad1f9af8a4109083b2ea4876
matikalo <matikalo@hotmail.com>
2021-10-19 18:03:17 -0300

    nosotros si foto

--- src/pages/Nosotros.vue
+++ src/pages/Nosotros.vue
@@ -6,6 +6,36 @@
       </div>
       <br>
       <br>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-2 col-1"></div>
+          <div class="col-md-9 col-10">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-8 col-12" style="text-align: left;">
+                  Ecorec es una empresa con 40 años de trayectoria ubicada en Buenos Aires, Argentina.<br>
+                  Originalmente siendo proveedor de servicio de lavandería especial para las industrias automotrices y farmacéuticas entre otras, ampliamos nuestra oferta con la elaboración de prendas y productos de primer nivel para diferentes industrias y el público en general.<br>
+                  Elaboramos equipos y vestuario laboral, destacando que somos especialistas en la provisión equipos de trabajo para áreas limpias, sean de la industria farmacéuticas o electrónica. A su vez ofrecemos una gama de productos para aquel cliente que cuente en sus instalaciones con entornos controlados o que necesite tener un entorno de trabajo con baja aportación de partículas y correctamente desinfectado.<br>
+                  Nuestros productos fueron desarrollados en conjunto con empresas internacionales, quienes nos proveen de toda la información y el material necesario para elaborarlos, y el aporte invaluable de nuestros clientes.<br>
+                  Ponemos a su disposición toda nuestra atención para ayudarlo en lo que necesite.<br>
+
+                </div>
+                <div class="col-md-4 col-12">
+                  <div class="contImgInst">
+                    <div class="imgMame"></div>
+                  <!--<img src="areaPharma/Foto_MameFarmaUSO.jpg" alt="foto_mame">-->
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-1"></div>
+        </div>
+      </div>
+
+      <br>
+      <br>
       <br>
       <div id="2">
 
