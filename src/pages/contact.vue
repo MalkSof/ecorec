9b9ac5b853fcd84f637329b6064a62310a6bc1e3
MalkSof <42155335+MalkSof@users.noreply.github.com>
2021-11-17 23:14:14 -0300

    contactgo

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="contacto">
         <NavComponent></NavComponent>
-        <div id="producto">
-          Contacto
-        </div>
+      <div class="nosotrosFinal">
+      <img src="contactoBanner.jpg" alt="productos" style="width: inherit; height: inherit; object-fit: cover">
+      </div>
         <a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
         <i class="fa fa-whatsapp my-float"></i>
         </a>
