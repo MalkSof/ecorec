b8719f1ca148521b6877245412c9206d936766bd
MalkSof <42155335+MalkSof@users.noreply.github.com>
2021-12-06 20:12:36 -0300

    center

--- src/components/SubNavComponent.vue
+++ src/components/SubNavComponent.vue
@@ -2,7 +2,7 @@
     <div>
       <div id="Group_54">
 
-      <form action="">
+
         <div class="container">
           <div class="row">
             <div class="col-md-12 col-12"  id="Contctanos">
@@ -37,12 +37,11 @@
           <label for="mensaje" id="Mensaje">Mensaje</label>
           <input type="text" class="input" name="mensaje" placeholder=" Tu mensaje..."><br><br>
           <div style="text-align: end;">
-            <input type="submit" id="Enviar" value="Enviar">
+            <input @click="enviarEmail" type="submit" id="Enviar" value="Enviar">
           </div>
         </div>
 
 
-      </form>
     </div>
         <div class="footer">
             <div class="container">
@@ -112,9 +111,93 @@
     </div>
 </template>
 
+
 <script>
+
+
+
+import axios from "axios";
+
 export default {
-  name: "SubNavComponent"
+  name: "SubNavComponent",
+  data()
+  {
+    return{
+      form:{
+        nombre : '',
+        apellido: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+
+      }
+    }
+  },
+  methods:
+  {
+    enviarEmail(){
+
+      var data = JSON.stringify(  {"personalizations": [{"to": [{"email": "nicolasuzalcman@gmail.com"}]}],"from":
+            {"email": "nicozalc@gmail.com"},"subject": "Sending with SendGrid is Fun","content":
+            [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]});
+
+      var url = {
+        method: 'post',
+        url: 'https://api.sendgrid.com/v3/mail/send',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 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'
+        },
+        data : data
+      };
+
+      //let myvalue =JSON.stringify({"Step2":[{"selected":!1,"produto":"A70-5","ncm":"9506.62.00","aliqII":.2,"aliqIpi":0,"aliqPis":.021,"aliqCofins":.0965,"descricao":"BOLA DE BASQUETE","pesoUnitario":.43,"unitario":.78,"dumping":0,"cubagemUnitario":0,"quantidade":150,"icmEstadoDestino":0,"mvaAjustado":0,"markup":.12},{"selected":!1,"produto":"PINK-7-103","ncm":"7117.90.00","aliqII":.18,"aliqIpi":.12,"aliqPis":.021,"aliqCofins":.0965,"descricao":"BIJOUTERIAS EM METAL/ VIDRO ","pesoUnitario":.975,"unitario":1.52,"dumping":0,"cubagemUnitario":0,"quantidade":200,"icmEstadoDestino":0,"mvaAjustado":0,"markup":.11},{"selected":!1,"produto":"PINK-7-103","ncm":"7117.90.00","aliqII":.18,"aliqIpi":.12,"aliqPis":.021,"aliqCofins":.0965,"descricao":"BIJOUTERIAS EM METAL/ VIDRO ","pesoUnitario":.975,"unitario":1.52,"dumping":0,"cubagemUnitario":0,"quantidade":300,"icmEstadoDestino":0,"mvaAjustado":0,"markup":.19}],});
+
+      fetch('https://api.sendgrid.com/v3/mail/send', {
+        method: 'post',
+        headers: {
+          'Accept':'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 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'
+        },
+        body: data
+      }).then(function(response){
+        return response.json(data);
+      }).then(function(text){
+        console.log(text);
+      }).catch(function (error){
+        console.error(error);
+      })
+        axios(url).then(function (response) {
+            console.log(JSON.stringify(response.data));
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      /*
+      axios.post('https://api.sendgrid.com/v3/mail/send', data, config)
+          .then(() => (alert('Mensaje enviado Con exito')))
+          .catch(function (error) {
+            if (error.response) {
+              // Request made and server responded
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message);
+            }
+
+          });
+
+       */
+
+    }
+  }
 }
 </script>
 
